352e2f36157dd234cb7b73a3cf079ce7b6c7dfc1
oorhunn <anilorhundemiroglu@gmail.com>
2020-09-03 23:41:12 +0300

    updatinnn

--- blog.py
+++ blog.py
@@ -20,7 +20,7 @@ def index():
     return render_template('blog/index.html', posts=posts)
 
 
-@bp.route('/create', methods=('GET','POST'))
+@bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
@@ -29,7 +29,7 @@ def create():
         error = None
 
         if not title:
-            error = 'Title is required'
+            error = 'Title is required.'
 
         if error is not None:
             flash(error)
@@ -37,11 +37,13 @@ def create():
             db = get_db()
             db.execute(
                 'INSERT INTO post (title, body, author_id)'
-                'VALUES (?,?,?)',
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
-        return render_template('blog/create.html')
+
+    return render_template('blog/create.html')
 
 
 def get_post(id, check_author=True):
@@ -59,4 +61,45 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
+
+@bp.route('/<int:id>/update', methods=('GET','POST'))
+@login_required
+def update(id):
+    global title, error, body
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+    if not title:
+        error = 'Title is required'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'UPDATE post SET title = ?, body = ?'
+            'WHERE id = ?',
+            (title, body, id)
+        )
+        db.commit()
+        return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
+
+
 
